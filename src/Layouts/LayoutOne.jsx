a3dcfcd8be88087ac12c8a1c13da4c8ce63de757
Rafin Hasan <168470414+Rafin-Hasan@users.noreply.github.com>
2024-09-10 05:35:56 +0600

    modified:   src/Layouts/LayoutOne.jsx

--- src/Layouts/LayoutOne.jsx
+++ src/Layouts/LayoutOne.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Navber from "../component/Navber/Navber";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const LayoutOne = () => {
+  const sliceUser = useSelector((state) => state.counter.value);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (sliceUser == null) {
+      navigate("/loginPage");
+    }
+  }, []);
   return (
     <>
       <Navber />
